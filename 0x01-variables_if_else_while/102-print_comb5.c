b92f292fee78db26cedebe87ba422939c62f2d48
DannyS641 <danieloluwanifemi2002@gmail.com>
2022-06-03 07:07:50 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,4 +17,12 @@ putchar((num1 % 10) + '0');
 putchar(' ');
 putchar((num2 / 10) + '0');
 putchar((num2 % 10) + '0');
-if (num1 == 98 
+if (num1 == 98 && num2 == 99)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
