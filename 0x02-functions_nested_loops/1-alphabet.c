51c80e674991352925e73bbe2c7dde8cfc95d484
DannyS641 <danieloluwanifemi2002@gmail.com>
2022-06-08 06:20:11 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -13,17 +13,3 @@ _putchar (alpha);
 }
 _putchar('\n');
 }
-#include <"putchar.h">
-/**
-* print_alphabet - prints the english alphabet from a-z.
-* Return: Nothing.
-*/
-void print_alphabet(void)
-{
-char c;
-for (c = 'a'; c <= 'z'; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
-}
