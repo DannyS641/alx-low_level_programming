4067d60f9a9df6ab6f9619cb15b1b7c55b2b919f
DannyS641 <danieloluwanifemi2002@gmail.com>
2022-06-08 05:21:59 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 int main(void)
 {
-_putchar();
+  {
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -11,5 +11,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return (0);
+ return (0);
+  }
 }
