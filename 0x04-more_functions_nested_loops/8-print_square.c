ec6e1e7e67221fa57b2ee85a35a109845b09ba4c
DannyS641 <danieloluwanifemi2002@gmail.com>
2022-06-09 18:36:19 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,7 +3,8 @@
 * print_square - a function that prints a square, followed by a new line
 * @size: size of both width and length
 * Return: a square made of '#'
-*/void print_square(int size)
+*/
+void print_square(int size)
 {
 int co, ro;
 if (size <= 0)
@@ -21,5 +22,5 @@ _putchar('#');
 }
 _putchar('\n');
 }
-}
+} 
 }
